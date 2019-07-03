a6a207e76bca3d71c439eb945e99d49054d8f98d
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-07-03 21:57:32 +0300

    set up project

---
+++ resources/views/partials/javascript.blade.php
@@ -0,0 +1,9 @@
+<script src="{{asset('assets/js/jquery-2.0.0.min.js')}}"></script> <!-- Jquery Library Call --> 
+<script src="{{asset('assets/plugins/prettyphoto/js/prettyphoto.js')}}"></script> <!-- PrettyPhoto Plugin --> 
+<script src="{{asset('assets/js/helper-plugins.js')}}"></script> <!-- Plugins --> 
+<script src="{{asset('assets/js/bootstrap.js')}}"></script> <!-- UI --> 
+<script src="{{asset('assets/js/waypoints.js')}}"></script> <!-- Waypoints --> 
+<script src="{{asset('assets/plugins/mediaelement/mediaelement-and-player.min.js')}}"></script> <!-- MediaElements --> 
+<script src="{{asset('assets/js/init.js')}}"></script> <!-- All Scripts --> 
+<script src="{{asset('assets/plugins/flexslider/js/jquery.flexslider.js')}}"></script> <!-- FlexSlider --> 
+<script src="{{asset('assets/plugins/countdown/js/jquery.countdown.min.js')}}"></script> <!-- Jquery Timer -->
