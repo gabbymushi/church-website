0e7dcfae7cf50d39ceaa2e7429a21aa60ab9a196
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-07-09 00:09:38 +0300

    get all news

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\News;
 
 class BlogController extends Controller
 {
@@ -13,7 +14,8 @@ class BlogController extends Controller
      */
     public function index()
     {
-        return view('blog.blog');
+        $data['news']=News::all();
+        return view('blog.blog',$data);
     }
 
     /**
