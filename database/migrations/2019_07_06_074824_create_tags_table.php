166f06965d9ba78f191f124a8d947bc35b8729e3
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-07-08 06:56:04 +0300

    add relation with table users

--- database/migrations/2019_07_06_074824_create_tags_table.php
+++ database/migrations/2019_07_06_074824_create_tags_table.php
@@ -18,7 +18,7 @@ class CreateTagsTable extends Migration
             $table->string('name');
             $table->bigInteger('user_id')->index()->nullable();
             $table->timestamps();
-            
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
