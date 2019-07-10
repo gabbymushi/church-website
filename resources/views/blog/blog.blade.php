a96ff5cda2705256138069bf7c38c7203e88bc90
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-07-10 21:13:32 +0300

    display all blog posts

--- resources/views/blog/blog.blade.php
+++ resources/views/blog/blog.blade.php
@@ -31,7 +31,20 @@
       <div class="container">
         <div class="row">
           <div class="col-md-9 posts-archive">
+          @foreach($news as $news)
             <article class="post">
+              <div class="row">
+                <div class="col-md-4 col-sm-4"> <a href="single-event.html"><img src="http://placehold.it/600x400&amp;text=IMAGE+PLACEHOLDER" alt="" class="img-thumbnail"></a> </div>
+                <div class="col-md-8 col-sm-8">
+                  <h3><a href="single-event.html">{{$news->title}}</a></h3>
+                  <span class="post-meta meta-data"> <span><i class="fa fa-calendar"></i> 28th Jan, 2014</span><span><i class="fa fa-archive"></i> <a href="#">Uncategorized</a></span> <span><a href="#"><i class="fa fa-comment"></i> 12</a></span></span>
+                  <p>{{$news->content}}</p>
+                  <p><a href="#" class="btn btn-primary">Continue reading <i class="fa fa-long-arrow-right"></i></a></p>
+                </div>
+              </div>
+            </article>
+            @endforeach
+            <!-- <article class="post">
               <div class="row">
                 <div class="col-md-4 col-sm-4"> <a href="single-event.html"><img src="http://placehold.it/600x400&amp;text=IMAGE+PLACEHOLDER" alt="" class="img-thumbnail"></a> </div>
                 <div class="col-md-8 col-sm-8">
@@ -52,18 +65,7 @@
                   <p><a href="#" class="btn btn-primary">Continue reading <i class="fa fa-long-arrow-right"></i></a></p>
                 </div>
               </div>
-            </article>
-            <article class="post">
-              <div class="row">
-                <div class="col-md-4 col-sm-4"> <a href="single-event.html"><img src="http://placehold.it/600x400&amp;text=IMAGE+PLACEHOLDER" alt="" class="img-thumbnail"></a> </div>
-                <div class="col-md-8 col-sm-8">
-                  <h3><a href="single-event.html">A sample post title</a></h3>
-                  <span class="post-meta meta-data"> <span><i class="fa fa-calendar"></i> 28th Jan, 2014</span><span><i class="fa fa-archive"></i> <a href="#">Uncategorized</a></span> <span><a href="#"><i class="fa fa-comment"></i> 12</a></span></span>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis egestas rhoncus. Donec facilisis fermentum sem, ac viverra ante luctus vel. Donec vel mauris quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis egestas rhoncus. Donec facilisis fermentum sem, ac viverra ante luctus vel. Donec vel mauris quam.</p>
-                  <p><a href="#" class="btn btn-primary">Continue reading <i class="fa fa-long-arrow-right"></i></a></p>
-                </div>
-              </div>
-            </article>
+            </article> -->
             <ul class="pagination">
               <li><a href="#"><i class="fa fa-chevron-left"></i></a></li>
               <li class="active"><a href="#">1</a></li>
