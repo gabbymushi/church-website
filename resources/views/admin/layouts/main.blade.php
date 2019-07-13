fa75f5a4d4ade5aa7f503874449c302c177470a6
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-07-13 10:38:01 +0300

    import ckeditor

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -1,4 +1,3 @@
-
 <!doctype html>
 <html lang="en">
 
@@ -6,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="Content-Language" content="en">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Analytics Dashboard - This is an example dashboard created using build-in elements and components.</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
     <meta name="description" content="This is an example dashboard created using build-in elements and components.">
@@ -21,9 +20,10 @@
     =========================================================
     * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
     -->
-<link href="{{asset('dashboard_panel/main.css')}}" rel="stylesheet">
-<link href="{{asset('css/toastr.min.css')}}" rel="stylesheet">
+    <link href="{{asset('dashboard_panel/main.css')}}" rel="stylesheet">
+    <link href="{{asset('css/toastr.min.css')}}" rel="stylesheet">
 </head>
+
 <body>
     <div class="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
         <div class="app-header header-shadow">
@@ -56,7 +56,8 @@
                         </span>
                     </button>
                 </span>
-            </div>    <div class="app-header__content">
+            </div>
+            <div class="app-header__content">
                 <div class="app-header-left">
                     <div class="search-wrapper">
                         <div class="input-holder">
@@ -84,7 +85,8 @@
                                 Settings
                             </a>
                         </li>
-                    </ul>        </div>
+                    </ul>
+                </div>
                 <div class="app-header-right">
                     <div class="header-btn-lg pr-0">
                         <div class="widget-content p-0">
@@ -120,74 +122,86 @@
                                 </div>
                             </div>
                         </div>
-                    </div>        
+                    </div>
                 </div>
             </div>
-        </div>        
-            
+        </div>
+
         <div class="app-main">
-                <div class="app-sidebar sidebar-shadow">
-                    <div class="app-header__logo">
-                        <div class="logo-src"></div>
-                        <div class="header__pane ml-auto">
-                            <div>
-                                <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
-                                    <span class="hamburger-box">
-                                        <span class="hamburger-inner"></span>
-                                    </span>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="app-header__mobile-menu">
+            <div class="app-sidebar sidebar-shadow">
+                <div class="app-header__logo">
+                    <div class="logo-src"></div>
+                    <div class="header__pane ml-auto">
                         <div>
-                            <button type="button" class="hamburger hamburger--elastic mobile-toggle-nav">
+                            <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
                                 <span class="hamburger-box">
                                     <span class="hamburger-inner"></span>
                                 </span>
                             </button>
                         </div>
                     </div>
-                    <div class="app-header__menu">
-                        <span>
-                            <button type="button" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
-                                <span class="btn-icon-wrapper">
-                                    <i class="fa fa-ellipsis-v fa-w-6"></i>
-                                </span>
-                            </button>
-                        </span>
-                    </div>    
-                    @include('admin.includes.menu')
-
-                </div>    
-                <div class="app-main__outer">
-                    <div class="app-main__inner">
-                                   
-                       
-                        
-                        @yield('content')
-                   
-                        
-                    </div>
-                    @include('admin/includes/footer')   
                 </div>
-              
+                <div class="app-header__mobile-menu">
+                    <div>
+                        <button type="button" class="hamburger hamburger--elastic mobile-toggle-nav">
+                            <span class="hamburger-box">
+                                <span class="hamburger-inner"></span>
+                            </span>
+                        </button>
+                    </div>
+                </div>
+                <div class="app-header__menu">
+                    <span>
+                        <button type="button" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
+                            <span class="btn-icon-wrapper">
+                                <i class="fa fa-ellipsis-v fa-w-6"></i>
+                            </span>
+                        </button>
+                    </span>
+                </div>
+                @include('admin.includes.menu')
+
+            </div>
+            <div class="app-main__outer">
+                <div class="app-main__inner">
+
+
+
+                    @yield('content')
+
+
+                </div>
+                @include('admin/includes/footer')
+            </div>
+
         </div>
     </div>
     <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
-<script type="text/javascript" src="{{asset('dashboard_panel/assets/scripts/main.js')}}"></script>
-<script type="text/javascript" src="{{asset('js/jquery.min.js')}}"></script>
-<script type="text/javascript" src="{{asset('js/toastr.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/jquery.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/toastr.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('assets/plugins/ckeditor5-build-classic/ckeditor.js')}}"></script>
+    <script type="text/javascript" src="{{asset('dashboard_panel/assets/scripts/main.js')}}"></script>
     <script type="text/javascript">
-        @if(Session::has('success')){
-          
-          toastr.success("{{Session::get('success')}}");
+        @if(Session::has('success')) {
 
-        }@elseif(Session::has('warning')){
-          toastr.warning("{{Session::get('warning')}}");  
+            toastr.success("{{Session::get('success')}}");
+
+        }
+        @elseif(Session::has('warning')) {
+            toastr.warning("{{Session::get('warning')}}");
         }
 
         @endif
-</script>
+        
+        ClassicEditor.create(document.querySelector('#newsEditor'))
+            .then(editor => {
+                editor.config.height='600px';
+                console.log(editor);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    </script>
 </body>
+
 </html>
