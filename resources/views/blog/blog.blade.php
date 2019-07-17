ce4221ffd44ae22a3a990ea98432d9ffaf12404f
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-07-17 21:52:07 +0300

    allow raw html

--- resources/views/blog/blog.blade.php
+++ resources/views/blog/blog.blade.php
@@ -38,7 +38,7 @@
                 <div class="col-md-8 col-sm-8">
                   <h3><a href="single-event.html">{{$news->title}}</a></h3>
                   <span class="post-meta meta-data"> <span><i class="fa fa-calendar"></i> {{date('jS F,Y',strtotime($news->created_at))}}</span><span><i class="fa fa-archive"></i> <a href="#">Uncategorized</a></span> <span><a href="#"><i class="fa fa-comment"></i> 12</a></span></span>
-                  {{$news->content}}
+                  {!!$news->content!!}
                   <p><a href="{{url('/blog/'.$news->id)}}" class="btn btn-primary">Continue reading <i class="fa fa-long-arrow-right"></i></a></p>
                 </div>
               </div>
