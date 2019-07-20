46d7e0dadfd4ef7102fb10fd38d6164342b910f9
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-07-20 10:40:09 +0300

    display image from db

--- resources/views/blog/blog.blade.php
+++ resources/views/blog/blog.blade.php
@@ -2,49 +2,55 @@
 @section('content')
 <!-- Start Nav Backed Header -->
 <div class="nav-backed-header parallax" style="background-image:url(http://placehold.it/1280x635&amp;text=IMAGE+PLACEHOLDER);">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <ol class="breadcrumb">
-            <li><a href="index.html">Home</a></li>
-            <li class="active">Blog</li>
-          </ol>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <ol class="breadcrumb">
+          <li><a href="index.html">Home</a></li>
+          <li class="active">Blog</li>
+        </ol>
       </div>
     </div>
   </div>
-  <!-- End Nav Backed Header --> 
-  <!-- Start Page Header -->
-  <div class="page-header">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <h1>Blog</h1>
-        </div>
+</div>
+<!-- End Nav Backed Header -->
+<!-- Start Page Header -->
+<div class="page-header">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Blog</h1>
       </div>
     </div>
   </div>
-  <!-- End Page Header --> 
-  <!-- Start Content -->
-  <div class="main" role="main">
-    <div id="content" class="content full">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-9 posts-archive">
+</div>
+<!-- End Page Header -->
+<!-- Start Content -->
+<div class="main" role="main">
+  <div id="content" class="content full">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-9 posts-archive">
           @foreach($news as $news)
-            <article class="post">
-              <div class="row">
-                <div class="col-md-4 col-sm-4"> <a href="{{url('/blog/'.$news->id)}}"><img src="http://placehold.it/600x400&amp;text=IMAGE+PLACEHOLDER" alt="" class="img-thumbnail"></a> </div>
-                <div class="col-md-8 col-sm-8">
-                  <h3><a href="single-event.html">{{$news->title}}</a></h3>
-                  <span class="post-meta meta-data"> <span><i class="fa fa-calendar"></i> {{date('jS F,Y',strtotime($news->created_at))}}</span><span><i class="fa fa-archive"></i> <a href="#">Uncategorized</a></span> <span><a href="#"><i class="fa fa-comment"></i> 12</a></span></span>
-                  {!!$news->content!!}
-                  <p><a href="{{url('/blog/'.$news->id)}}" class="btn btn-primary">Continue reading <i class="fa fa-long-arrow-right"></i></a></p>
-                </div>
+          <article class="post">
+            <div class="row">
+              <div class="col-md-4 col-sm-4"> <a href="{{url('/blog/'.$news->id)}}">
+                  @if(!empty($news->files))
+                  <img src="{{asset('assets/images/blog/'.$news->files[0]['name'])}}" alt="" class="img-thumbnail">
+                  @else
+                  <img src="http://placehold.it/600x400&amp;text=IMAGE+PLACEHOLDER" alt="" class="img-thumbnail">
+                  @endif
+                </a> </div>
+              <div class="col-md-8 col-sm-8">
+                <h3><a href="single-event.html">{{$news->title}}</a></h3>
+                <span class="post-meta meta-data"> <span><i class="fa fa-calendar"></i> {{date('jS F,Y',strtotime($news->created_at))}}</span><span><i class="fa fa-archive"></i> <a href="#">Uncategorized</a></span> <span><a href="#"><i class="fa fa-comment"></i> 12</a></span></span>
+                {!!$news->content!!}
+                <p><a href="{{url('/blog/'.$news->id)}}" class="btn btn-primary">Continue reading <i class="fa fa-long-arrow-right"></i></a></p>
               </div>
-            </article>
-            @endforeach
-            <!-- <article class="post">
+            </div>
+          </article>
+          @endforeach
+          <!-- <article class="post">
               <div class="row">
                 <div class="col-md-4 col-sm-4"> <a href="single-event.html"><img src="http://placehold.it/600x400&amp;text=IMAGE+PLACEHOLDER" alt="" class="img-thumbnail"></a> </div>
                 <div class="col-md-8 col-sm-8">
@@ -66,45 +72,45 @@
                 </div>
               </div>
             </article> -->
-            <ul class="pagination">
-              <li><a href="#"><i class="fa fa-chevron-left"></i></a></li>
-              <li class="active"><a href="#">1</a></li>
-              <li><a href="#">2</a></li>
-              <li><a href="#">3</a></li>
-              <li><a href="#">4</a></li>
-              <li><a href="#">5</a></li>
-              <li><a href="#"><i class="fa fa-chevron-right"></i></a></li>
+          <ul class="pagination">
+            <li><a href="#"><i class="fa fa-chevron-left"></i></a></li>
+            <li class="active"><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            <li><a href="#">5</a></li>
+            <li><a href="#"><i class="fa fa-chevron-right"></i></a></li>
+          </ul>
+        </div>
+        <!-- Start Sidebar -->
+        <div class="col-md-3 sidebar">
+          <div class="widget sidebar-widget search-form-widget">
+            <div class="input-group input-group-lg">
+              <input type="text" class="form-control" placeholder="Search Posts...">
+              <span class="input-group-btn">
+                <button class="btn btn-default" type="button"><i class="fa fa-search fa-lg"></i></button>
+              </span> </div>
+          </div>
+          <div class="widget sidebar-widget">
+            <div class="sidebar-widget-title">
+              <h3>Post Categories</h3>
+            </div>
+            <ul>
+              <li><a href="#">Faith</a> (10)</li>
+              <li><a href="#">Missions</a> (12)</li>
+              <li><a href="#">Salvation</a> (34)</li>
+              <li><a href="#">Worship</a> (14)</li>
             </ul>
           </div>
-          <!-- Start Sidebar -->
-          <div class="col-md-3 sidebar">
-            <div class="widget sidebar-widget search-form-widget">
-              <div class="input-group input-group-lg">
-                <input type="text" class="form-control" placeholder="Search Posts...">
-                <span class="input-group-btn">
-                <button class="btn btn-default" type="button"><i class="fa fa-search fa-lg"></i></button>
-                </span> </div>
+          <div class="widget sidebar-widget">
+            <div class="sidebar-widget-title">
+              <h3>Post Tags</h3>
             </div>
-            <div class="widget sidebar-widget">
-              <div class="sidebar-widget-title">
-                <h3>Post Categories</h3>
-              </div>
-              <ul>
-                <li><a href="#">Faith</a> (10)</li>
-                <li><a href="#">Missions</a> (12)</li>
-                <li><a href="#">Salvation</a> (34)</li>
-                <li><a href="#">Worship</a> (14)</li>
-              </ul>
-            </div>
-            <div class="widget sidebar-widget">
-              <div class="sidebar-widget-title">
-                <h3>Post Tags</h3>
-              </div>
-              <div class="tag-cloud"> <a href="#">Faith</a> <a href="#">Heart</a> <a href="#">Love</a> <a href="#">Praise</a> <a href="#">Sin</a> <a href="#">Soul</a> <a href="#">Missions</a> <a href="#">Worship</a> <a href="#">Faith</a> <a href="#">Heart</a> <a href="#">Love</a> <a href="#">Praise</a> <a href="#">Sin</a> <a href="#">Soul</a> <a href="#">Missions</a> <a href="#">Worship</a> </div>
-            </div>
+            <div class="tag-cloud"> <a href="#">Faith</a> <a href="#">Heart</a> <a href="#">Love</a> <a href="#">Praise</a> <a href="#">Sin</a> <a href="#">Soul</a> <a href="#">Missions</a> <a href="#">Worship</a> <a href="#">Faith</a> <a href="#">Heart</a> <a href="#">Love</a> <a href="#">Praise</a> <a href="#">Sin</a> <a href="#">Soul</a> <a href="#">Missions</a> <a href="#">Worship</a> </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-  @endsection
+</div>
+@endsection
