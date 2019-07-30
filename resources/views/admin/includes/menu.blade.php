1b4fbce50eb30c5c57a51c08c0b26e731ae790e7
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-07-30 11:48:38 +0300

    add dashboard url

--- resources/views/admin/includes/menu.blade.php
+++ resources/views/admin/includes/menu.blade.php
@@ -2,8 +2,8 @@
     <div class="app-sidebar__inner">
         <ul class="vertical-nav-menu">
             <li>
-                <a href="index.html" class="mm-active">
-                    <i class="metismenu-icon pe-7s-rocket"></i>
+                <a href="{{url('/dashboard')}}" class="mm-active">
+                    <i class="metismenu-icon fa fa-tachometer"></i>
                     Dashboard
                 </a>
             </li>
