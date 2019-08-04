78b80bdb6fc11d42dc75513ae7bc86589b185490
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-08-04 09:06:44 +0300

    view ministry details

--- app/Http/Controllers/MinistryController.php
+++ app/Http/Controllers/MinistryController.php
@@ -55,7 +55,7 @@ class MinistryController extends Controller
      */
     public function show($id)
     {
-        return view('');
+        return view('ministry.view-ministry');
     }
 
     /**
